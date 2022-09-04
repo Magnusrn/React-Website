dd24cb2280f574c2e87b4428173106fa65a67566
magnus <magnusn@hotmail.co.uk>
2022-09-04 08:42:09 +0100

    Terminal - add description and styling

--- src/pages/Terminal.js
+++ src/pages/Terminal.js
@@ -5,6 +5,8 @@ function Terminal() {
     return (
         <div>
             <h1 className={styles.title}>Terminal</h1>
+            <h6 className={styles.description}>Input help to see available commands</h6>
+            <h6 className={styles.description}> Also supports arithmetic and !! for previous command</h6>
             <RenderTerminal/>
         </div>
     );
